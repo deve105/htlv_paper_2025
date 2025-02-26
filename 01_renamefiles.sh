8ac893f3b1b7e4ee784c2fe6fefb4d3c09e24d5c
DEV-iOS <daniel.enriquez.v@upch.pe>
2025-02-26 22:25:45 +0900

    ws

--- 01_renamefiles.sh
+++ 01_renamefiles.sh
@@ -2,7 +2,7 @@
 
 for file in *.txt; do
     # Remove "clonality_", "_rmdup", and leading digits with an underscore
-    new_name=$(echo "$file" | sed -E 's/_clonality.*//; s/_rmdup.*//; s/^[0-99]_//').txt
+    new_name=$(echo "$file" | sed -E 's/_clonality.*//; s/_rmdup.*//; s/^[0-9]{2}_//').txt
     
     # Rename the file only if the new name is different
     if [[ "$file" != "$new_name" ]]; then
