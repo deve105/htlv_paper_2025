941cb71927c7df736f72f982002998d0487f2020
Daniel Enriquez-Vera <daniel.enriquez.v@upch.pe>
2025-04-21 16:53:40 +0900

    ws

---
+++ 2504_IS.R
@@ -0,0 +1,45 @@
+## Packages
+pacman::p_load(
+    tidyverse, tidyplots
+)
+
+## Loading file
+overall_coverage = read_tsv("./data/2504_mosdepth_global.tsv", col_names=FALSE)
+
+overall_cov = overall_coverage |>
+    dplyr::rename(chrom=1, length=2, bases=3, median=4, ID=7) |>
+    dplyr::mutate(median=as.numeric(median), 
+        less=ifelse(as.numeric(median)<5, "LC", "OK"),
+        peru=ifelse(str_detect(ID,"IRID|SS"), "Peru", "Japan"),
+        ID=str_replace_all(ID, "2._", "")) |>
+        dplyr::filter(chrom=="total", peru=="Peru") 
+
+overall_cov |>
+    dplyr::filter(median<5)
+
+
+overall_cov |>
+    summarise(
+    n = n(),
+    mean = mean(median, na.rm = TRUE),
+    sd = sd(median, na.rm = TRUE),
+    se = sd / sqrt(n),
+    lower_ci = mean - qt(0.975, df = n - 1) * se,  # 95% CI lower bound
+    upper_ci = mean + qt(0.975, df = n - 1) * se   # 95% CI upper bound
+  )
+
+"""
+A total of 71 samples were sequenced. The mean HTLV-1 coverage in the Peruvian cohort was 191× (95% CI: 156–226). Samples with less than 5× coverage—IRID038, IRID011, IRID040, IRID032, IRID006, IRID016, IRID086, and IRID067—were excluded. Among these, four harbored type II (5’LTR-deleted) defective viruses. The global defective virus rate was 6.3%, with no type I defective viruses detected.
+""" 
+overall_cov |>
+        dplyr::mutate(median=median+1 ) |>
+        tidyplots::tidyplot(y=ID, x=median) |>
+        tidyplots::add_median_bar(width=.5) |>
+        tidyplots::sort_y_axis_labels(.reverse=TRUE) |>
+        tidyplots::adjust_y_axis_title("Sample ID", face="bold") |>
+        tidyplots::adjust_x_axis_title("Whole HTLV-1 Coverage per Sample\n log10(Coverage + 1)", face="bold") |>
+        tidyplots::adjust_x_axis(transform = "log10", labels = scales::trans_format("log10", scales::math_format(10^.x))) |>
+        tidyplots::adjust_font(fontsize = 6) |>
+        tidyplots::add_reference_lines(x = 191, linetype = "dashed", linewidth = .5, color="red") |>
+        tidyplots::adjust_size(width = 5, height = 13, unit = "cm") |>
+        tidyplots::save_plot("2504_HTLV1coverage.png", bg="transparent")
